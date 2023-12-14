70bbb48b98b061da97f36eaf82ff441386e5ae9f
Federico Muzzo <federico.muzzo@bitrock.it>
2023-12-14 19:04:24 +0100

    part 2 solution with path finding

--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,97 +1,69 @@
-use itertools::Itertools;
-use memoize::memoize;
+use pathfinding::{cycle_detection::brent, matrix::Matrix};
 
 use crate::{Solution, SolutionPair};
 use std::fs::read_to_string;
 
 ///////////////////////////////////////////////////////////////////////////////
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Rock {
     Round,
     Square,
     Empty,
 }
 
-#[memoize]
-fn push_north(initial_map: String) -> String {
-    let mut map = initial_map
-        .split('\n')
-        .map(|r| {
-            r.chars()
-                .map(|c| match c {
-                    'O' => Rock::Round,
-                    '#' => Rock::Square,
-                    _ => Rock::Empty,
-                })
-                .collect_vec()
-        })
-        .collect_vec();
-
-    let rocks = &map
-        .iter()
-        .enumerate()
-        //.par_bridge()
-        .map(|(y, row)| {
-            row.iter()
-                .enumerate()
-                //.par_bridge()
-                .filter(|(_, rock)| match rock {
-                    Rock::Round => true,
-                    _ => false,
-                })
-                .map(move |(x, _)| (x.clone(), y.clone()))
-        })
-        .flatten()
-        .collect_vec();
-
-    for (x, y) in rocks.iter() {
-        let mut column: Vec<Rock> = vec![];
-        let boundary: usize = *y;
-        for i in 0..boundary {
-            let rock = &map[i][*x];
-            column.push(*rock);
-        }
-        column.reverse();
-
-        let spaces = column
-            .iter()
-            .take_while(|r| **r != Rock::Square)
-            .filter(|r| **r == Rock::Empty)
-            .count();
-        if spaces > 0 {
-            map[*y][*x] = Rock::Empty;
-            map[*y - spaces][*x] = Rock::Round;
+impl std::fmt::Display for Rock {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Rock::Round => write!(f, "O"),
+            Rock::Square => write!(f, "#"),
+            Rock::Empty => write!(f, "."),
         }
     }
-
-    let new = map
-        .iter()
-        .map(|row| {
-            row.iter()
-                .map(|rock| match rock {
-                    Rock::Round => 'O',
-                    Rock::Square => '#',
-                    _ => '.',
-                })
-                .collect::<String>()
-        })
-        .join("\n");
-
-    new
 }
 
-fn calculate_pressure(map: String) -> u64 {
-    let height: usize = map.split('\n').count();
+fn cycle(grid: &mut Matrix<Rock>) {
+    for _ in 0..4 {
+        tilt(grid);
+        grid.rotate_cw(1);
+    }
+}
 
-    map.split('\n')
+fn tilt(grid: &mut Matrix<Rock>) {
+    for x in 0..grid.columns {
+        let mut free = 0;
+        for y in 0..grid.rows {
+            match grid[(y, x)] {
+                Rock::Square => free = y + 1,
+                Rock::Round => {
+                    grid.swap((y, x), (free, x));
+                    free += 1;
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+fn parse_grid(input: String) -> Matrix<Rock> {
+    Matrix::from_rows(input.lines().map(|r| {
+        r.chars().map(|c| match c {
+            '.' => Rock::Empty,
+            '#' => Rock::Square,
+            'O' => Rock::Round,
+            _ => panic!("Invalid input"),
+        })
+    }))
+    .unwrap()
+}
+
+fn calculate_pressure(grid: &Matrix<Rock>) -> u64 {
+    grid.iter()
         .enumerate()
         .map(|(y, r)| {
-            let weight = y;
-            r.chars()
-                .map(|c| match c {
-                    'O' => height as u64 - weight as u64,
-                    '#' => 0,
+            r.iter()
+                .map(|r| match r {
+                    Rock::Round => grid.columns as u64 - y as u64,
                     _ => 0,
                 })
                 .sum::<u64>()
@@ -99,13 +71,29 @@ fn calculate_pressure(map: String) -> u64 {
         .sum::<u64>()
 }
 
+fn part2(grid: &mut Matrix<Rock>) -> u64 {
+    const LIMIT: usize = 1_000_000_000;
+    let (len, mut last, idx) = brent(grid.clone(), |mut g| {
+        cycle(&mut g);
+        g
+    });
+    for _ in 0..(LIMIT - idx) % len {
+        cycle(&mut last);
+    }
+    calculate_pressure(&last)
+}
+
 pub fn solve() -> SolutionPair {
     let input = read_to_string("input/days/day14.txt").unwrap();
+    let mut grid = parse_grid(input.clone());
+    tilt(&mut grid);
 
-    // Your solution here...
-    let sol1: u64 = calculate_pressure(push_north(input.clone()));
+    let sol1: u64 = calculate_pressure(&grid);
 
-    let sol2: u64 = 0;
+    /* let mut grid_2 = parse_grid(input.clone());
+    let sol2: u64 = part_two(&grid_2); */
+    let mut grid = parse_grid(input.clone());
+    let sol2 = part2(&mut grid);
 
     (Solution::from(sol1), Solution::from(sol2))
 }
